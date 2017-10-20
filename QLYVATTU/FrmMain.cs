127b80d7d6b6624d40f294795bdf3161f7930309
PhilipLam95 <drakaabc456@gmail.com>
2017-10-20 06:12:29 -0700

    20/10

--- QLYVATTU/FrmMain.cs
+++ QLYVATTU/FrmMain.cs
@@ -19,6 +19,8 @@ namespace QLYVATTU
             InitializeComponent();
         }
 
+        private Datatable dasdsa;
+
         //check form 
         private Form CheckExists(Type ftype)
         {
