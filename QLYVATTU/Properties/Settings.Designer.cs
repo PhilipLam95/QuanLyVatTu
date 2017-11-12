74d0bffb6b3a4343f2702666aaf8abdbf738637b
NgocThanhqn <ngocthanhqn95@gmail.com>
2017-11-12 16:57:52 +0700

    sssss

--- QLYVATTU/Properties/Settings.Designer.cs
+++ QLYVATTU/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QLYVATTU.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -41,6 +41,16 @@ namespace QLYVATTU.Properties {
         public string QL_VATTUConnectionString {
             get {
                 return ((string)(this["QL_VATTUConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NGOC-THANH-IS;Initial Catalog=QL_VATTU;User ID=sa")]
+        public string QL_VATTUConnectionString1 {
+            get {
+                return ((string)(this["QL_VATTUConnectionString1"]));
             }
         }
     }
